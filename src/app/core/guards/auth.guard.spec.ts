e5de1aa12a0c2f75bf56408c8691fc279d4b21f0
romanrfhack <romanrfhack@gmail.com>
2026-02-09 18:51:56 -0600

    Fix auth guard unit tests

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { provideRouter, Router } from '@angular/router';
+import { provideRouter, Route, Router, UrlSegment } from '@angular/router';
 import { AuthService } from '../../features/auth/services/auth.service';
 import { authGuard } from './auth.guard';
 
@@ -18,8 +18,30 @@ describe('authGuard', () => {
     });
 
     const router = TestBed.inject(Router);
-    const result = TestBed.runInInjectionContext(() => authGuard());
+    const route = { path: 'app' } as Route;
+    const segments: UrlSegment[] = [];
+    const result = TestBed.runInInjectionContext(() => authGuard(route, segments));
 
     expect(result).toEqual(router.createUrlTree(['/login']));
+  });
+
+  it('should allow access when user is authenticated', () => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideRouter([]),
+        {
+          provide: AuthService,
+          useValue: {
+            isAuthenticated: () => true,
+          },
+        },
+      ],
+    });
+
+    const route = { path: 'app' } as Route;
+    const segments: UrlSegment[] = [];
+    const result = TestBed.runInInjectionContext(() => authGuard(route, segments));
+
+    expect(result).toBe(true);
   });
 });
