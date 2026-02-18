3b9e833948b775a26dc5bfd4df65e4deb2b243f0
Román Romero Figueroa <romanrfhack@gmail.com>
2026-02-18 13:51:48 -0600

    Correccion de fix back

--- backend/src/CobranzaDigital.Infrastructure/Migrations/20260218192012_disponibilidad.cs
+++ backend/src/CobranzaDigital.Infrastructure/Migrations/20260218192012_disponibilidad.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace CobranzaDigital.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class disponibilidad : Migration
+    public partial class Disponibilidad : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
